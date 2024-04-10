33a62835aa44eb41c5390bf013f0deabd844b670
jaiswalrahul2427 <jaiswalrahul2427@gamil.com>
2024-04-10 10:56:11 +0530

    connecting to database

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({ path: './env' })
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './.env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+const app = express();
+(async() => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        application.on("error", (error) => {
+            console.log("ERROR the", error);
+            throw error
+        })
+        aap.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err
+
+    }
+})()*/
